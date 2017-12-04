da8997d53403de02bb93e91b0ad20a5bd0762831
seunzone <darealseun@gmail.com>
2017-12-04 16:08:49 +0100

    setup config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'YOUR-TEAM-NAME',
+  community: process.env.COMMUNITY_NAME || 'Aspiring Andelans',
   // your slack team url (ex: socketio.slack.com)
-  slackUrl: process.env.SLACK_URL || 'YOUR-TEAM.slack.com',
+  slackUrl: process.env.SLACK_URL || 'https://join.slack.com/t/aspiringandelans/shared_invite/enQtMjc3NjQ4MjcwNzU0LWE1ZWFhZWQzOWI1Y2IxMzhiNzg0NjkxMzNlY2EyOWU1ZDM4YTQ4NzNhYTc0ZDkwYzE1ODdkOTI4ZGY5ZDM4YTA',
   // access token of slack
   // You can generate it in https://api.slack.com/web#auth
   // You should generate the token in admin user, not owner.
@@ -12,7 +12,7 @@ module.exports = {
   //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
   //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
   //   --compressed
-  slacktoken: process.env.SLACK_TOKEN || 'YOUR-ACCESS-TOKEN',
+  slacktoken: process.env.SLACK_TOKEN || 'xoxp-278522529702-277792104165-281289041523-796dbd19e90c0f7262b9d846bf0f9d4d',
   // an optional security measure - if it is set, then that token will be required to get invited.
   inviteToken: process.env.INVITE_TOKEN || null,
   // an optional security measure - if both are set, then recaptcha will be used.
