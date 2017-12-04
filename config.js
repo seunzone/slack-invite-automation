ab1283c9d9e2b2fe8ab73a88317d73225a490a07
seunzone <darealseun@gmail.com>
2017-12-04 16:17:50 +0100

    fix token issue

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
   //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
   //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
   //   --compressed
-  slacktoken: process.env.SLACK_TOKEN || 'xoxp-278522529702-277792104165-281289041523-796dbd19e90c0f7262b9d846bf0f9d4d',
+  slacktoken: process.env.SLACK_TOKEN || 'YOUR-ACCESS-TOKEN',
   // an optional security measure - if it is set, then that token will be required to get invited.
   inviteToken: process.env.INVITE_TOKEN || null,
   // an optional security measure - if both are set, then recaptcha will be used.
